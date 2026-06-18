0f1dc4b7c5c3c25836d15c39313c21dd0ab7eea1
dante6686-pp <dante6686@gmail.com>
2026-06-18 09:06:14 +0200

    Add questions data to questions.js

---
+++ questions.js
@@ -0,0 +1,26 @@
+export const QUESTIONS = [
+  {
+    id: 1,
+    text: "Woda wrze w temperaturze 90°C.",
+    options: [
+      "Temperatura 90°C",
+      "Woda",
+      "Wrzenie",
+      "Brak błędu"
+    ],
+    correct: 0,
+    explanation: "Woda wrze w 100°C (na poziomie morza)."
+  },
+  {
+    id: 2,
+    text: "Na fladze USA znajduje się 51 gwiazd.",
+    options: [
+      "USA",
+      "51 gwiazd",
+      "Flaga",
+      "Brak błędu"
+    ],
+    correct: 1,
+    explanation: "USA ma 50 stanów, więc 50 gwiazd."
+  }
+];
